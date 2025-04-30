802ac5610640a0389e2e2b50fe6547152e0e93f6
AntBelyi <66726924+AntBelyi@users.noreply.github.com>
2025-04-30 17:10:07 +0300

    Revert "Add Homework1"

--- src/main/java/hw_1/Program.java
+++ src/main/java/hw_1/Program.java
@@ -5,15 +5,16 @@ public class Program {
     public static void main(String[] args) {
         int a = 7;
         int b = 9;
-        System.out.println("befoure a " + a);
-        System.out.println("befoure b " + b);
 
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        System.out.println("Before changing");
+        System.out.println(a);
+        System.out.println(b);
 
-        System.out.println("After a " + a);
-        System.out.println("After b " + b);
+        //**some code**//
+
+        System.out.println("After changing");
+        System.out.println(9);
+        System.out.println(7);
     }
 
 }

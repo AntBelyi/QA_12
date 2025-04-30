43b1358ccfb52395cd7caa4e07b6afa36e1071fe
Anna Veselova <ganna.veselova@gmail.com>
2025-04-30 22:22:21 +0300

    Homework_2 second part

---
+++ src/main/java/hw_2/Task_2.java
@@ -0,0 +1,20 @@
+package hw_2;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        String string = "Testing, is my favourite job";
+        int a = string.indexOf(' ');
+        System.out.println(a);
+        String[] words = string.split(" ");
+        for (String val : words) {
+            System.out.println(val + " length " + val.length());
+        }
+        for (int i = 1; i < words.length; i++) {
+            if (a < words[i].length()) {
+                System.out.println(" true ");
+            }
+            System.out.println(" false");
+            break;
+        }
+    }
+}

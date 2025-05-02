465b72d2056bcbb588f604277b496c8b3591bf0c
Anna Veselova <ganna.veselova@gmail.com>
2025-05-02 20:59:39 +0300

    i corrected the mistakes

--- src/main/java/hw_2/Task_2.java
+++ src/main/java/hw_2/Task_2.java
@@ -2,19 +2,20 @@ package hw_2;
 
 public class Task_2 {
     public static void main(String[] args) {
-        String string = "Testing, is my favourite job";
-        int a = string.indexOf(' ');
-        System.out.println(a);
+        String string = "Testing, is my favourite job".replace(",", "");
         String[] words = string.split(" ");
+        String FirstWord = words[0];
+        System.out.println(FirstWord);
+        boolean result = true;
         for (String val : words) {
             System.out.println(val + " length " + val.length());
         }
         for (int i = 1; i < words.length; i++) {
-            if (a < words[i].length()) {
-                System.out.println(" true ");
+            if (FirstWord.length() < words[i].length()) {
+                result = false;
+                System.out.println(result);
+                break;
             }
-            System.out.println(" false");
-            break;
         }
     }
 }
--- src/main/java/hw_2/Task_3.java
+++ src/main/java/hw_2/Task_3.java
@@ -3,7 +3,6 @@ package hw_2;
 public class Task_3 {
     public static void main(String[] args) {
         String string = "Completely random text in English. In it, we just need to determine how man times the character 'a' occurs there. And we can use the split method and the length method.";
-        int b = string.length();
         String[] arrayOfString = string.split("");
         int number = 0;
         for (String val : arrayOfString) {

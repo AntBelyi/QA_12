64b49d40ed7803cc84c4769444107f8d92d482c7
Anna Veselova <ganna.veselova@gmail.com>
2025-05-03 12:07:51 +0300

    Add a correction HW_2

--- src/main/java/hw_2/Task_2.java
+++ src/main/java/hw_2/Task_2.java
@@ -2,20 +2,20 @@ package hw_2;
 
 public class Task_2 {
     public static void main(String[] args) {
-        String string = "Testing, is my favourite job".replace(",", "");
+        String string = "Testing, is my favourite job";
         String[] words = string.split(" ");
-        String FirstWord = words[0];
-        System.out.println(FirstWord);
+        String firstWord = words[0].replace(",", "");
+        System.out.println(firstWord);
         boolean result = true;
         for (String val : words) {
             System.out.println(val + " length " + val.length());
         }
         for (int i = 1; i < words.length; i++) {
-            if (FirstWord.length() < words[i].length()) {
+            if (firstWord.length() < words[i].length()) {
                 result = false;
-                System.out.println(result);
                 break;
             }
         }
+        System.out.println(result);
     }
 }

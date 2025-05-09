9c633d924cecad39ee4ccf009ab106e5155802bc
yulia29-git <eremenkojuliia29@gmail.com>
2025-05-09 17:18:48 +0300

    Fix_hw2

--- src/main/java/hw2/task_2.java
+++ src/main/java/hw2/task_2.java
@@ -5,21 +5,14 @@ public class task_2 {
         String sentence = "Testing, is my favourite job".replace(",","");
 
         String[] words = sentence.split(" ");
-
-        int firstWord= words[0].length();
+        boolean isFirstLonger = true;
+        int firstWordLength = words[0].length();
 
         for (int i = 0; i < words.length; i++) {
             int wordLength = words[i].length();
-            boolean isFirstLonger;
-
-            if (i == 0) {
-                isFirstLonger = true;
-            } else {
-                isFirstLonger = firstWord > wordLength;
-            }
+            if (firstWordLength < wordLength) { isFirstLonger = false;}
 
             System.out.println("Слово" + (i + 1) + " = (" + words[i] + "), Довжина цього слова = (" + wordLength + "), " + isFirstLonger);
         }
     }
 }
-

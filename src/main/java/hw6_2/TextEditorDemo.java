82fcc3a804eabcb860e1cdb68885eff9e0cf2b0c
Даниил <d.solomchenko2020@gmail.com>
2025-05-31 14:58:59 +0300

    Add Homework6_2

--- src/main/java/hw6_2/TextEditorDemo.java
+++ src/main/java/hw6_2/TextEditorDemo.java
@@ -1,4 +1,55 @@
-package hw6_2;
+// Інтерфейс TextProcessor
+interface TextProcessor {
+    String process(String text);
 
+    default String removeDigits(String text) {
+        return text.replace(".", "");
+    }
+}
+
+// Клас UpperCaseProcessor
+class UpperCaseProcessor implements TextProcessor {
+    @Override
+    public String process(String text) {
+        return text.toUpperCase();
+    }
+}
+
+// Клас ReverseProcessor
+class ReverseProcessor implements TextProcessor {
+    @Override
+    public String process(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
+}
+
+// Клас TrimProcessor
+class TrimProcessor implements TextProcessor {
+    @Override
+    public String process(String text) {
+        return text.trim();
+    }
+}
+
+// Головний клас
 public class TextEditorDemo {
+    public static void main(String[] args) {
+        String sampleText = "  Hello World.123. ";
+
+        TextProcessor upper = new UpperCaseProcessor();
+        TextProcessor reverse = new ReverseProcessor();
+        TextProcessor trim = new TrimProcessor();
+
+        System.out.println("--- UpperCaseProcessor ---");
+        System.out.println("Processed: " + upper.process(sampleText));
+        System.out.println("Without dots: " + upper.removeDigits(sampleText));
+
+        System.out.println("\n--- ReverseProcessor ---");
+        System.out.println("Processed: " + reverse.process(sampleText));
+        System.out.println("Without dots: " + reverse.removeDigits(sampleText));
+
+        System.out.println("\n--- TrimProcessor ---");
+        System.out.println("Processed: " + trim.process(sampleText));
+        System.out.println("Without dots: " + trim.removeDigits(sampleText));
+    }
 }

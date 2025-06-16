939719dae23ecf2b3f9566f771f62f8408f49d7f
Anton_Bilyi <Anton_Bilyi@epam.com>
2025-06-16 12:24:26 +0300

    minor changes

--- src/main/java/hw_10/Task_1/Application.java
+++ src/main/java/hw_10/Task_1/Application.java
@@ -1,17 +1,20 @@
-package Task_1;
+package hw_10.Task_1;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Application {
-    public static void currentDateTime(String[] args) {
+
+    public static void main(String[] args) {
+        currentDateTime();
+    }
+
+    public static void currentDateTime() {
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println(localDateTime);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         System.out.println("formatter:" + " " + localDateTime.format(formatter));
     }
-    public static void main(String[] args) {
-        currentDateTime(args);
-    }
+
 }
--- src/main/java/hw_10/Task_3/Application.java
+++ src/main/java/hw_10/Task_3/Application.java
@@ -1,16 +1,18 @@
-package Task_3;
+package hw_10.Task_3;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 public class Application {
-    public static boolean isWeekend(LocalDateTime dateTime) {
-        DayOfWeek day = dateTime.getDayOfWeek();
-        return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
-    }
 
     public static void main(String[] args) {
         System.out.println("25.05.2024 — Is weekend? " + isWeekend(LocalDateTime.of(2024, 5, 25, 10, 0)));
         System.out.println("08.09.2025 — Is weekend? " + isWeekend(LocalDateTime.of(2025, 9, 8, 10, 0)));
     }
+
+    public static boolean isWeekend(LocalDateTime dateTime) {
+        DayOfWeek day = dateTime.getDayOfWeek();
+        return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
+    }
+
 }
